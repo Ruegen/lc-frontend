0f3bc5b90ba19b3e92dca4d8a5922e22f8153212
ruegen <ruegen@icloud.com>
2018-07-30 23:36:33 +1000

    add production env

--- src/api/init.js
+++ src/api/init.js
@@ -5,8 +5,10 @@ export default {
     fetchCouncils
 }
 
+const baseURL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_API_URL : process.env.REACT_APP_DEV_API_URL
+
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: baseURL
 })
 
 // TODO: tests
